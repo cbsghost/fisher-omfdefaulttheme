e8cb51d7dbe7e0c602854b2930dd406d575f2be7
Radovan Bast <bast@users.noreply.github.com>
2019-07-13 20:20:43 +0200

    simplify and adjust to my taste

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -5,6 +5,7 @@
 function fish_prompt
   set -l last_command_status $status
   set -l cwd
+  echo -n \n
 
   if test "$theme_short_path" = 'yes'
     set cwd (basename (prompt_pwd))
@@ -12,24 +13,15 @@ function fish_prompt
     set cwd (prompt_pwd)
   end
 
-  set -l fish     "⋊>"
-  set -l ahead    "↑"
-  set -l behind   "↓"
-  set -l diverged "⥄ "
-  set -l dirty    "⨯"
-  set -l none     "◦"
+  set -l ahead    " (ahead)"
+  set -l behind   " (behind)"
+  set -l diverged " (diverged)"
+  set -l dirty    " (modified)"
+  set -l none     ""
 
   set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
   set -l directory_color  (set_color $fish_color_quote 2> /dev/null; or set_color brown)
   set -l repository_color (set_color $fish_color_cwd 2> /dev/null; or set_color green)
-
-  if test $last_command_status -eq 0
-    echo -n -s $success_color $fish $normal_color
-  else
-    echo -n -s $error_color $fish $normal_color
-  end
 
   if git_is_repo
     if test "$theme_short_path" = 'yes'
@@ -38,8 +30,8 @@ function fish_prompt
       set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
     end
 
-    echo -n -s " " $directory_color $cwd $normal_color
-    echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
+    echo -n -s $directory_color $cwd $normal_color
+    echo -n -s " on " $repository_color (git_branch_name) $normal_color
 
     if git_is_touched
       echo -n -s $dirty
@@ -47,8 +39,8 @@ function fish_prompt
       echo -n -s (git_ahead $ahead $behind $diverged $none)
     end
   else
-    echo -n -s " " $directory_color $cwd $normal_color
+    echo -n -s $directory_color $cwd $normal_color
   end
 
-  echo -n -s " "
+  echo -s \n "\$ "
 end
